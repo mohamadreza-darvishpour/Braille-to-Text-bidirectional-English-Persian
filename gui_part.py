b60b494f77c74b63bbdb898530709a2981360de9
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2024-07-30 19:49:41 +0330

    pyqt5 docs

--- gui_part.py
+++ gui_part.py
@@ -1,5 +1,290 @@
+https://pythonpyqt.com/pyqt-box-layout/
 
 
+
+
+
+
+
+
+# import sys
+# from PyQt5.QtWidgets import (QApplication, QWidget, QToolTip, QPushButton)
+# from PyQt5.QtGui import QFont
+
+# class Example(QWidget):
+  
+#   def __init__(self):
+#     super().__init__()
+#     self.initUI()
+  
+#   def initUI(self):
+#     QToolTip.setFont(QFont('Arial', 14))
+#     self.setToolTip('Tooltip for <b>QWidget</b>')
+#     btn = QPushButton('Button', self)
+#     btn.setToolTip('Tooltip for <b>QPushButton</b>')
+#     btn.resize(btn.sizeHint())
+#     btn.move(50, 50)
+#     self.setGeometry(300, 300, 300, 220)
+#     self.setWindowTitle('PyQt Tooltip Demo')    
+#     self.show()
+  
+# if __name__ == '__main__':
+#   app = QApplication(sys.argv)
+#   ex = Example()
+#   sys.exit(app.exec_())
+
+
+
+
+
+#custom signals.
+# import sys
+# from PyQt5.QtWidgets import QApplication, QMainWindow
+# from PyQt5.QtCore import pyqtSignal, QObject
+ 
+# class Communicate(QObject):    #defines our custom signals here.
+#     closeApp = pyqtSignal()
+ 
+# class Example(QMainWindow):
+
+#     def __init__(self):
+#         super().__init__()
+#         self.initUI()
+#     def initUI(self):
+#         self.c = Communicate()
+#         self.c.closeApp.connect(self.close)
+#         self.setGeometry(300, 300, 300, 150)
+#         self.setWindowTitle('Mouse events') 
+#         self.show()
+    
+#     def mousePressEvent(self, event):   #mousepressevent is custom class in app
+#         self.c.closeApp.emit()
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+# # how to get and enable changes . 
+# import sys
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+# class Example(QMainWindow):
+
+#     def __init__(self):
+#          super().__init__()
+#          self.initUI()
+
+#     def initUI(self):
+#         btn1 = QPushButton('One', self)
+#         btn1.move(30, 50)
+#         btn2 = QPushButton('Two', self)
+#         btn2.move(150, 50)
+#         btn1.clicked.connect(self.buttonClicked)   # when want to connect that to functions.
+#         btn2.clicked.connect(self.buttonClicked)
+#         self.statusBar()
+#         self.setGeometry(300, 300, 300, 150)
+#         self.setWindowTitle('Window') 
+#         self.show()
+
+#     def buttonClicked(self):
+#         sender = self.sender()
+#         self.statusBar().showMessage(sender.text() + ' changed')
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+
+# #set custom events. 
+# import sys
+# from PyQt5.QtWidgets import QApplication, QWidget
+# from PyQt5.QtCore import Qt
+
+# class Example(QWidget):
+
+#     def __init__(self):
+#         super().__init__()
+#         self.initUI()
+        
+#     def initUI(self):
+#         self.setGeometry(300, 300, 250, 150)
+#         self.setWindowTitle('PyQt events with keyboard') 
+#         self.show()
+        
+#     def keyPressEvent(self, e):
+#         if e.key() == Qt.Key_Escape:
+#             self.close()
+    
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+
+
+# #events is important to use .
+
+# import sys
+
+# from PyQt5.QtWidgets import (QApplication, QWidget, QSlider, QLCDNumber, QVBoxLayout)
+# from PyQt5.QtCore import Qt
+
+# class Example(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         self.initUI()
+        
+#     def initUI(self):
+#        lcd = QLCDNumber(self)
+#        sld = QSlider(Qt.Horizontal, self)
+#        vbox = QVBoxLayout()
+#        vbox.addWidget(lcd)   #add lcd(widget ) to slider
+#        vbox.addWidget(sld)  #add sld to slider too
+#        self.setLayout(vbox)  #set layout . 
+#        sld.valueChanged.connect(lcd.display) #when change the sld show on lcd. 
+#        self.setGeometry(250, 250, 250, 150)
+#        self.setWindowTitle('Events demo') 
+#        self.show()
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+
+
+# from PyQt5.QtWidgets import *
+# import sys
+
+# class QLabelBuddy(QDialog):
+#     def __init__(self):
+#         super().__init__()
+#         self.initUI()
+
+#     def initUI(self):
+#         self.setWindowTitle('QLabel Buddy Demo')
+#         self.setFixedWidth(250)
+
+#         nameLabel = QLabel('&Name',self)
+#         nameLineEdit = QLineEdit(self)
+#         nameLabel.setBuddy(nameLineEdit)
+
+#         passwordLabel = QLabel('&Password',self)
+#         passwordLineEdit = QLineEdit(self)
+#         passwordLabel.setBuddy(passwordLineEdit)
+
+#         btnOK = QPushButton('&OK')
+#         btnCancel = QPushButton('&Cancel')
+
+#         mainLayout = QGridLayout(self)
+#         mainLayout.addWidget(nameLabel,0,0)
+#         mainLayout.addWidget(nameLineEdit,0,1,1,2)
+
+#         mainLayout.addWidget(passwordLabel,1,0)
+#         mainLayout.addWidget(passwordLineEdit,1,1,1,2)
+
+#         mainLayout.addWidget(btnOK,2,1)
+#         mainLayout.addWidget(btnCancel,2,2)
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     main = QLabelBuddy()
+#     main.show()
+#     sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import sys
+# from PyQt5.QtWidgets import QVBoxLayout, QMainWindow, QApplication, QLabel, QWidget
+# from PyQt5.QtGui import QPixmap, QPalette
+# from PyQt5.QtCore import Qt
+
+# class QLabelDemo(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         self.initUI()
+
+#     def initUI(self):
+#         label1 = QLabel(self)
+#         label2 = QLabel(self)
+#         label3 = QLabel(self)
+#         label4 = QLabel(self)
+
+#         label1.setText("<font color=yellow>PythonPyQt.com</font>")
+#         label1.setAutoFillBackground(True)
+#         palette = QPalette()
+#         palette.setColor(QPalette.Window,Qt.blue)
+#         label1.setPalette(palette)
+#         label1.setAlignment(Qt.AlignCenter)
+
+#         label2.setText("<a href='#'>QLabel2</a>")
+
+#         label3.setAlignment(Qt.AlignCenter)
+#         label3.setToolTip('Hint')
+#         label3.setPixmap(QPixmap("python.png"))
+
+#         label4.setOpenExternalLinks(True)
+#         label4.setText("<a href='https://python.org'>Python.org</a>")
+#         label4.setAlignment(Qt.AlignCenter)
+#         label4.setToolTip('Python.org')
+
+#         vbox = QVBoxLayout()
+
+#         vbox.addWidget(label1)
+#         vbox.addWidget(label2)
+#         vbox.addWidget(label3)
+#         vbox.addWidget(label4)
+
+#         label2.linkHovered.connect(self.linkHovered)
+#         label4.linkActivated.connect(self.linkClicked)
+
+#         self.setLayout(vbox)
+#         self.setWindowTitle('QLabel Demo')
+
+#     def linkHovered(self):
+#         print('Mouse hovered over the link')
+
+#     def linkClicked(self):
+#         print('Link was clicked')
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     main = QLabelDemo()
+#     main.show()
+#     sys.exit(app.exec_())
 
 
 
